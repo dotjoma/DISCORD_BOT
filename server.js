9867e19d4cd6ba088f3687f2d1180066b1954d01
yikes <79975300+dotjoma@users.noreply.github.com>
2024-02-10 15:51:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const server = express();
 
+const currentDate = new Date();
+
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth() + 1; // Months are zero-indexed
+const day = currentDate.getDate();
+const hour = currentDate.getHours();
+const minute = currentDate.getMinutes();
+const second = currentDate.getSeconds();
+
+const formattedDateTime = `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+
 server.all('/', (req, res) => {
-  res.send('Bot: [RUNNING].');
+  res.send('Bot Status: [RUNNING].', formattedDateTime);
 });
 
 
 function keepAlive() {
   server.listen(3000, () => {
-    console.log('Server is now ready! | ' + Date.now());
+    console.log('Server is now ready! | ', formattedDateTime);
   });
 }
 
